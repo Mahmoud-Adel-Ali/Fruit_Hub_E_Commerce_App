f8cb8846b3c35c06a6e0be60bfcdb55687b78e71
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-07-15 21:04:25 -0700

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub_e_commerce_app/fruit_hub_app.dart';
 
 import 'core/databases/cach_helper.dart';
 import 'core/services/service_locator.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServicesLocator();
   await getit.get<CacheHelper>().init();
   runApp(const FruitHubApp());
