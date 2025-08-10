2ef52bb58dd7800999554e0c8791c561f0531480
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-08-09 23:36:58 -0700

    47. Implement create user with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,1 +1,28 @@
-class FirebaseAuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../errors/exceptions.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+          message: 'The account already exists for that email.',
+        );
+      } else {
+        throw CustomException(message: 'Failed to create user , try again.');
+      }
+    } catch (e) {
+      throw CustomException(message: 'Failed to create user , try again.');
+    }
+  }
+}
