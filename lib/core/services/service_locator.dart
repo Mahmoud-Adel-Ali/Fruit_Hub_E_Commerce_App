96d8f19831ebbb8ed85835516618529b392b2cad
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-08-24 20:14:56 -0700

    Use Getit to provide signup cubit

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,8 +1,16 @@
 import 'package:get_it/get_it.dart';
 
+import '../../features/auth/data/repos/auth_repo_impl.dart';
+import '../../features/auth/domain/repos/auth_repo.dart';
 import '../databases/cach_helper.dart';
+import 'firebase_auth_service.dart';
 
 final getit = GetIt.instance;
+
 void setupServicesLocator() {
   getit.registerSingleton<CacheHelper>(CacheHelper());
+  getit.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getit.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthService: getit<FirebaseAuthService>()),
+  );
 }
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/services/service_locator.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import '../../domain/repos/auth_repo.dart';
+import '../cubits/signup_cubit/signup_cubit.dart';
 import 'widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -9,9 +13,12 @@ class SignupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBar(context, title: 'حساب جديد', showLeading: false),
-      body: const SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(authRepo: getit<AuthRepo>()),
+      child: Scaffold(
+        appBar: customAppBar(context, title: 'حساب جديد', showLeading: false),
+        body: const SignupViewBody(),
+      ),
     );
   }
 }
