1717f0cb4752c195e61eb086715dd1f4afb5a7f9
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-08-29 14:51:29 -0700

    53. Add Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub_e_commerce_app/fruit_hub_app.dart';
 
+import 'core/app/app_bloc_observer.dart';
 import 'core/databases/cach_helper.dart';
 import 'core/services/service_locator.dart';
 import 'firebase_options.dart';
@@ -9,6 +11,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = AppBlocObserver();
   setupServicesLocator();
   await getit.get<CacheHelper>().init();
   runApp(const FruitHubApp());
