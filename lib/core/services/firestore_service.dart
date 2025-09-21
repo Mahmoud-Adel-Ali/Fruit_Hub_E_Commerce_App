5c99d3b332982951d520388fb4823ecfdab899f8
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-09-21 15:11:02 -0700

    Create Database & Firestore Service.

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,9 @@
+import 'database_service.dart';
+
+class FirestoreService implements DatabaseService {
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {}
+}
