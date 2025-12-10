60dc2400d56ead3e282cbd6a142613e895b730e8
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-12-10 05:49:11 -0800

    Add Method to Products Repo.

--- lib/core/repos/products_repo/products_repo.dart
+++ lib/core/repos/products_repo/products_repo.dart
@@ -1,1 +1,9 @@
-class ProductsRepo {}
+import 'package:dartz/dartz.dart';
+
+import '../../entities/product_entity.dart';
+import '../../errors/failures.dart';
+
+abstract class ProductsRepo {
+  Future<Either<Failure, List<ProductEntity>>> getProducts();
+  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts();
+}
