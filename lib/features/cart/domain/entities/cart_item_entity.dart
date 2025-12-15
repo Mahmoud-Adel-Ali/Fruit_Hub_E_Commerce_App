b029af8498cfc6e77b335d4091bf03356451d471
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-12-15 09:49:31 -0800

    `Added CartItemEntity and updated related widgets to use it`

---
+++ lib/features/cart/domain/entities/cart_item_entity.dart
@@ -0,0 +1,18 @@
+import '../../../../core/entities/product_entity.dart';
+
+class CartItemEntity {
+  int quantity;
+  final ProductEntity product;
+
+  CartItemEntity({required this.product, this.quantity = 0});
+
+  num get totalPrice => product.price * quantity;
+  num get totalWeight => product.unitAmount * quantity;
+
+  CartItemEntity copyWith({int? quantity, ProductEntity? product}) {
+    return CartItemEntity(
+      quantity: quantity ?? this.quantity,
+      product: product ?? this.product,
+    );
+  }
+}
