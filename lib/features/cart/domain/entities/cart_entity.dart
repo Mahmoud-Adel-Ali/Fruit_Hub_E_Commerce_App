7e812d42b14c64b676848adbf4f24960389f53d8
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-12-15 10:10:45 -0800

    Cart Entity Part two.

--- lib/features/cart/domain/entities/cart_entity.dart
+++ lib/features/cart/domain/entities/cart_entity.dart
@@ -4,4 +4,33 @@ class CartEntity {
   final List<CartItemEntity> cartItems;
 
   CartEntity({required this.cartItems});
+
+  //* Add item to cart
+  void addItemToCart(CartItemEntity cartItem) {
+    //* If the item is in the cart, update the quantity
+    //* else add it to the cart
+    if (isItemInCart(cartItem)) {
+      cartItems
+          .firstWhere((item) => item.product.code == cartItem.product.code)
+          .increaseQuantity();
+    } else {
+      cartItems.add(cartItem);
+    }
+  }
+
+  //* Decrease item quantity in cart
+  void decreaseItemQuantity(CartItemEntity cartItem) {
+    cartItems
+        .firstWhere((item) => item.product.code == cartItem.product.code)
+        .decreaseQuantity();
+  }
+
+  //* Remove item from cart
+  void removeItemFromCart(CartItemEntity cartItem) {
+    cartItems.removeWhere((item) => item.product.code == cartItem.product.code);
+  }
+
+  //* Check the item in cart
+  bool isItemInCart(CartItemEntity cartItem) =>
+      cartItems.any((item) => item.product.code == cartItem.product.code);
 }
