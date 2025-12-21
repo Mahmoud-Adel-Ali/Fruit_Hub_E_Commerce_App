dc045e06b758fa70ec6a88c4b95257b9eab67861
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-12-21 06:14:56 -0800

    Custom Phone Text Field

---
+++ lib/core/widgets/custom_phone_text_form_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../helper_functions/validation_of_input_fields.dart';
+import '../utils/app_colors.dart';
+import 'custom_text_form_field.dart';
+
+class CustomPhoneTextFormField extends StatefulWidget {
+  const CustomPhoneTextFormField({
+    super.key,
+    this.hintText = 'رقم الهاتف',
+    this.controller,
+  });
+  final String hintText;
+  final TextEditingController? controller;
+
+  @override
+  State<CustomPhoneTextFormField> createState() =>
+      _CustomPhoneTextFormFieldState();
+}
+
+class _CustomPhoneTextFormFieldState extends State<CustomPhoneTextFormField> {
+  bool isValid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      hintText: 'رقم الهاتف',
+      controller: widget.controller,
+      keyboardType: TextInputType.phone,
+      validator: validatorOfEgyptianPhone,
+      onChanged: (value) {
+        isValid = validatorOfEgyptianPhoneBool(value);
+        setState(() {});
+      },
+      prefixIcon: Icon(
+        Icons.phone,
+        color: isValid ? AppColors.primaryColor : AppColors.grey,
+      ),
+      suffixIcon: isValid
+          ? Icon(Icons.check, color: AppColors.primaryColor)
+          : null,
+    );
+  }
+}
