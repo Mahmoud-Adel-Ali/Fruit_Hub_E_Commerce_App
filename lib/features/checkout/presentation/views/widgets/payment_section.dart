861c1460c361e0bf9e70ea0af7dc866853e2647e
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-12-23 22:09:45 -0800

    `Added new payment item and spacing to PaymentSection widget` [Location Info].

--- lib/features/checkout/presentation/views/widgets/payment_section.dart
+++ lib/features/checkout/presentation/views/widgets/payment_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_text_styles.dart';
 import 'order_info_item.dart';
 import 'payment_item.dart';
 
@@ -22,6 +24,36 @@ class PaymentSection extends StatelessWidget {
                 OrderInfoItem(title: "الكلي", price: 160.00, isTotal: true),
               ],
             ),
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        PaymentItem(
+          title: 'عنوان التوصيل',
+          child: Row(
+            children: [
+              const SizedBox(width: 8.0),
+              Icon(Icons.location_on_outlined, color: AppColors.grey),
+              const SizedBox(width: 8.0),
+              Text(
+                'شارع النيل، مبنى رقم ١٢٣',
+                style: AppTextStyles.regular16.copyWith(
+                  color: AppColors.grey.shade500,
+                ),
+              ),
+              const Spacer(),
+              TextButton.icon(
+                onPressed: () {},
+                label: Text(
+                  "تعديل",
+                  style: AppTextStyles.bold13.copyWith(color: AppColors.grey),
+                ),
+                icon: Icon(
+                  Icons.edit_outlined,
+                  size: 16.0,
+                  color: AppColors.grey,
+                ),
+              ),
+            ],
           ),
         ),
       ],
